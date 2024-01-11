f4c40d37283c87a1154aa4636d71d3ae713ec7f6
skn437 <skn437physx@gmail.com>
2024-01-11 17:45:31 +0600

    Added Config File For Environment Values

--- src/main.go
+++ src/main.go
@@ -1,10 +1,12 @@
 package main
 
-import (
-	"skn-go-mail/src/pkgs/routes"
-)
+import "skn-go-mail/src/pkgs/routes"
 
 func main() {
 	routes.GetRoutes()
+
+	//var base string = utils_functions.ViperEnvVariable("ROUTES.MAIL.BASIC")
+
+	//fmt.Printf("Base: %s", base)
 
 }
--- src/pkgs/utils/router.go
+++ src/pkgs/utils/router.go
@@ -1,4 +1,4 @@
-package utils_functions
+package utils
 
 import "github.com/gin-gonic/gin"
 
